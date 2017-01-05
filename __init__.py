2d174bb06bbc07d96edcb0e9b6feb54597b203ac
plumex <icewiner@gmail.com>
2017-01-06 02:07:17 +0800

    [Add] add python wraper.

---
+++ __init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ctypes import cdll, c_char, c_char_p, cast, POINTER
+import os.path
+
+class Thulac:
+    def __init__(self, model_path='', user_dict_path='', pre_alloc_size=1024*1024*16, t2s=False, just_seg=False):
+        path = os.path.dirname(os.path.realpath(__file__))
+        if len(model_path) == 0:
+            model_path = path+'/models'
+
+        self.lib = cdll.LoadLibrary(path+'/libthulac.so')
+        self.lib.getResult.restype = POINTER(c_char)
+        self.lib.init(c_char_p(model_path), c_char_p(user_dict_path), pre_alloc_size, int(t2s), int(just_seg)) 
+
+    def cut(self, text):
+        r = self.lib.seg(c_char_p(text))
+        p = self.lib.getResult()
+        s = cast(p,c_char_p)
+        d = '%s'%s.value
+        self.lib.freeResult();
+        return d.split(' ')
